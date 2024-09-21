d06071935de871581b93c89857ac624d5782cc92
Kateryna-1 <katerynasalata@icloud.com>
2024-09-21 16:59:11 +0200

    add solution

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,58 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor (questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
 
-    // 2. getQuestion()
+    }
+
+    getQuestion() {
+        return this.questions[this.currentQuestionIndex];
+    }
     
-    // 3. moveToNextQuestion()
+    moveToNextQuestion() {
+        if(this.currentQuestionIndex < this.questions.length - 1) {
+            this.currentQuestionIndex++;
+        }
+    }
 
-    // 4. shuffleQuestions()
+    shuffleQuestions() {
+        for (let i = this.questions.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]];
+        }
+    }
 
-    // 5. checkAnswer(answer)
+    checkAnswer(answer) {
+        const currentQuestion = this.getQuestion();
+        if (currentQuestion.answer === answer) {
+            this.correctAnswers++;
+            return true;  
+        } else {
+            return false; 
+        }
+    }
 
-    // 6. hasEnded()
-}
+    hasEnded() {
+        return this.currentQuestionIndex >= this.questions.length;
+    }
+
+    filterQuestionsByDifficulty(difficulty) { 
+
+        if (difficulty >= 1 && difficulty <= 3) {
+            this.questions = this.questions.filter((question) => {
+                return question.difficulty === difficulty
+            });
+        }
+
+    }
+
+    averageDifficulty() {
+        const total = this.questions.reduce((acc, curr) => {
+            return acc + curr.difficulty;
+        }, 0);
+        return total / this.questions.length;
+    }
+};
